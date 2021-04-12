47e641e45f2e2812a6ad402fa0bd173da04a7c7c
JuergenSchwertl <JuergenSchwertl@users.noreply.github.com>
2021-04-12 12:37:11 +0200

    WIP

--- main.c
+++ main.c
@@ -70,20 +70,20 @@ typedef enum _errnum_t {
 static const pwm_groups uPwmGroup_RgbLed = OS_HAL_PWM_GROUP2;
 
 
-//static const uint8_t pwm_channel_led_red = PWM_CHANNEL0;
-//static const uint8_t pwm_bitmap_led_red = OS_HAL_PWM_0;
-//static const uint32_t pwm_frequency_led_red = 20000;
-//static uint32_t pwm_duty_led_red = 300;
-//
-//static const uint8_t pwm_channel_led_green = PWM_CHANNEL1;
-//static const uint8_t pwm_bitmap_led_green = OS_HAL_PWM_1;
-//static const uint32_t pwm_frequency_led_green = 20000;
-//static uint32_t pwm_duty_led_green = 600;
+static const pwm_channels pwm_channel_led_red = PWM_CHANNEL0;
+static const pwms_bit_map pwm_bitmap_led_red = OS_HAL_PWM_0;
+static const uint32_t pwm_frequency_led_red = 2;
+static uint32_t pwm_duty_led_red = 300;
+
+static const pwm_channels pwm_channel_led_green = PWM_CHANNEL1;
+static const pwms_bit_map pwm_bitmap_led_green = OS_HAL_PWM_1;
+static const uint32_t pwm_frequency_led_green = 2;
+static uint32_t pwm_duty_led_green = 600;
 
 static const pwm_channels pwm_channel_led_blue = PWM_CHANNEL2;
 static const pwms_bit_map pwm_bitmap_led_blue = OS_HAL_PWM_2;
-static const uint32_t pwm_frequency_led_blue = 20000;
-static uint32_t pwm_duty_led_blue = 900;
+static const uint32_t pwm_frequency_led_blue = 20;
+static uint32_t pwm_duty_led_blue = 500;
 
 
 
@@ -91,59 +91,62 @@ static uint32_t pwm_duty_led_blue = 900;
 /* Define thread prototypes.  */
 void    main_thread(ULONG thread_input);
 
-/* Define main entry point.  */
 
-int main(void)
-{
-     /* Enter the ThreadX kernel.  */
-    tx_kernel_enter();
-}
 
 
 int init_hardware(void)
 {
-    log_debug("ends");
+    log_debug("starts\n");
     // initialize hardware
+    int iRet = 0;
 
+    volatile int iWait = 1;
+    while (iWait == 0)
+    {
+        __asm__("nop");
+    }
+
+    iRet != mtk_os_hal_pwm_ctlr_init(uPwmGroup_RgbLed, pwm_bitmap_led_red | pwm_bitmap_led_green | pwm_bitmap_led_blue);
     /* Init PWM */
-    if (mtk_os_hal_pwm_ctlr_init(uPwmGroup_RgbLed, pwm_bitmap_led_blue) != 0) {
+    if(iRet != 0) {
         log_err("mtk_os_hal_pwm_ctlr_init failed\n");
-        return ERR_HARDWARE_INIT;
+        //return ERR_HARDWARE_INIT;
     }
 
     /* Configure PWM */
-
-    if (mtk_os_hal_pwm_feature_enable(uPwmGroup_RgbLed,
-        pwm_channel_led_blue,
-        GLOBAL_KICK,
-        IO_CTRL,
-        POLARITY_SET) != 0)
+    iRet |= mtk_os_hal_pwm_feature_enable(uPwmGroup_RgbLed, pwm_channel_led_red, GLOBAL_KICK, IO_CTRL, POLARITY_SET);
+    iRet |= mtk_os_hal_pwm_feature_enable(uPwmGroup_RgbLed, pwm_channel_led_green, GLOBAL_KICK, IO_CTRL, POLARITY_SET);
+    iRet |= mtk_os_hal_pwm_feature_enable(uPwmGroup_RgbLed, pwm_channel_led_blue, GLOBAL_KICK, IO_CTRL, POLARITY_SET);
+    if ( iRet != 0)
     {
         log_err("mtk_os_hal_pwm_feature_enable failed\n");
-        return ERR_HARDWARE_INIT;
+        //return ERR_HARDWARE_INIT;
     }
 
-    if (mtk_os_hal_pwm_config_freq_duty_normal(uPwmGroup_RgbLed,
-        pwm_channel_led_blue,
-        pwm_frequency_led_blue,
-        pwm_duty_led_blue) != 0)
+    iRet |= mtk_os_hal_pwm_config_freq_duty_normal(uPwmGroup_RgbLed, pwm_channel_led_red, pwm_frequency_led_red, pwm_duty_led_red);
+    iRet |= mtk_os_hal_pwm_config_freq_duty_normal(uPwmGroup_RgbLed, pwm_channel_led_green, pwm_frequency_led_green, pwm_duty_led_green);
+    iRet |= mtk_os_hal_pwm_config_freq_duty_normal(uPwmGroup_RgbLed, pwm_channel_led_blue, pwm_frequency_led_blue, pwm_duty_led_blue);
+    if( iRet != 0)
     {
         log_err("mtk_os_hal_pwm_config_freq_duty_normal failed\n");
-        return ERR_HARDWARE_INIT;
+        //return ERR_HARDWARE_INIT;
     }
 
-    if (mtk_os_hal_pwm_start_normal(uPwmGroup_RgbLed,
-        pwm_channel_led_blue) != 0) {
+    iRet |= mtk_os_hal_pwm_start_normal(uPwmGroup_RgbLed, pwm_channel_led_red);
+    iRet |= mtk_os_hal_pwm_start_normal(uPwmGroup_RgbLed, pwm_channel_led_green);
+    iRet |= mtk_os_hal_pwm_start_normal(uPwmGroup_RgbLed, pwm_channel_led_blue);
+    if (iRet != 0) {
         log_err("mtk_os_hal_pwm_start_normal failed\n");
-        return ERR_HARDWARE_INIT;
+        //return ERR_HARDWARE_INIT;
     }
 
-    log_debug("ends");
-    return STATUS_OK;
+    log_debug("ends\n");
+    return iRet;
 }
 
 void    tx_application_define(void* first_unused_memory)
 {
+    log_debug("starts\n");
 
     CHAR* pointer;
 
@@ -176,7 +179,7 @@ void    main_thread(ULONG thread_input)
     /* This thread simply sits in while-forever-blink loop.  */
     while (1)
     {
-        pwm_duty_led_blue += 1;
+        pwm_duty_led_blue += 100;
 		if (pwm_duty_led_blue > 1000)
 			pwm_duty_led_blue = 0;
 
@@ -185,9 +188,18 @@ void    main_thread(ULONG thread_input)
 							pwm_frequency_led_blue,
 							pwm_duty_led_blue);
 
-        /* Sleep for 10 ticks == 1/10 sec.  */
-        tx_thread_sleep(10);
+        /* Sleep for 25 ticks == 25/100 = 1/4  sec.  */
+        tx_thread_sleep(25);
     }
+
+    log_debug("ends\n");
 }
 
 
+int main(void)
+{
+    /* Enter the ThreadX kernel.  */
+    tx_kernel_enter();
+
+    return STATUS_OK;
+}
